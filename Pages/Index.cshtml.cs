330edd5ac95795678af84a47d5f0bbc5f8fa7859
Wojciech Wojtach <wojtachwojciech5@gmail.com>
2026-05-23 11:50:00 +0200

    panel ze statystykami

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Magazyn.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,10 +8,21 @@ namespace Magazyn.Pages;
 public class IndexModel : PageModel
 {
     private readonly Db _db;
+    public int TotalProducts { get; set; }
+    public int LowStock { get; set; }
+    public int TotalOperations { get; set; }
+    public List<Product> Products { get; set; } = new();
 
     public IndexModel(Db db) => _db = db;
 
     public void OnGet()
     {
+        var data = _db.Load();
+        Products = data.Products;
+
+        // podstawowe statystyki na strone glowna
+        TotalProducts = data.Products.Count;
+        LowStock = data.Products.Count(p => p.Quantity < p.MinQuantity);
+        TotalOperations = data.Operations.Count;
     }
 }
